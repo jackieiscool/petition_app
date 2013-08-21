e9dea2464ec3408f92b62d9334e9a7a8af3f8ba7
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-08-21 15:20:41 -0700

    signature model done, come back to specs

--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -5,7 +5,7 @@ describe Signature do
   it { should belong_to :petition }
 
   it { should validate_presence_of :petition }
-  it { should validate :user_or_info }
-  it { should validate_uniqueness_of :user_id, scope: :petition_id }
-  it { should validate_uniqueness_of :petition_id, scope: [:first_name, :last_name]}
+  # it { should validate :user_or_info }
+  # it { should validate_uniqueness_of(:user_id).scoped_to(:petition_id) }
+  # it { should validate_uniqueness_of(:petition_id).scoped_to(:first_name, :last_name) }
 end
