e180d0e4cf1fecd76b51c37859cb818ebaab7e5e
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-08-21 11:28:25 -0700

    done with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  
+  has_many :petitions
+  has_many :signatures
 end
